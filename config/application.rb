4a61c9ca5f2574aa1762b703e656e92e039838a4
alvaro <aaruizf@unal.edu.co>
2021-02-10 07:12:30 -0500

    Fix some linter errors

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,11 @@
-require_relative "boot"
-
-require "rails/all"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+# frozen_string_literal: true
+
+require_relative 'boot'
+
+require 'rails/all'
+
 Bundler.require(*Rails.groups)
 
 module Twitty
